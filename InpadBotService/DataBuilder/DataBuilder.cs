153c6f529959183641e3f42fc14ff1419c730092
Александр Щербаков <sasaserbakov812@gmail.com>
2024-11-16 13:49:00 +0500

    da2

--- InpadBotService/DataBuilder/DataBuilder.cs
+++ InpadBotService/DataBuilder/DataBuilder.cs
@@ -4,5 +4,13 @@ namespace InpadBotService.DataBuilder;
 
 public class DataBuilder
 {
+    public DataBuilder()
+    {
 
+    }
+
+    public void UpdateData(UserData user, string message, string additionalParam)
+    {
+        user.data.Append()
+    }
 }
--- InpadBotService/DataBuilder/UserData.cs
+++ InpadBotService/DataBuilder/UserData.cs
@@ -1,13 +1,16 @@
 using System.Text;
 
+
 namespace InpadBotService.DataBuilder;
 
 public class UserData
 {
     public StringBuilder data;
+    public DataBuilder builder;
 
     public UserData()
     {
         data = new StringBuilder();
     }
+
 }
