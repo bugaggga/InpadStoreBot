47b0652927986805ec4d5a1ed3eae9f490de453f
Александр Щербаков <sasaserbakov812@gmail.com>
2024-12-19 16:10:50 +0500

    add jsonhandler

---
+++ InpadBotService/JSONMethods/JSONFileHandler.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InpadBotService.JSONMethods
+{
+    internal class JSONFileHandler
+    {
+        string JSON;
+
+        public JSONFileHandler(string fileName)
+        {
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                JSON = r.ReadToEnd();
+            }
+        }
+    }
+}
